32e080d40beba111bbcaa4879cc7ff0fb3e3c078
jurbunic <jbunic94@gmail.com>
2017-04-18 19:45:50 +0200

    Promjena početne stranice
    -Na početnoj stranici se sada odabire jezik aplikacije
    -Dodane klasa Lokalizacija u kojoj se obrađuje promjena lokalizacije
    - Dodana klasa Izbornik
    - Dodan faces-config.xml

---
+++ src/java/org/foi/nwtis/jurbunic/web/zrna/Lokalizacija.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.foi.nwtis.jurbunic.web.zrna;
+
+import javax.inject.Named;
+import javax.enterprise.context.SessionScoped;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Locale;
+import javax.faces.component.UIViewRoot;
+import javax.faces.context.FacesContext;
+import org.foi.nwtis.jurbunic.web.kontrole.Izbornik;
+
+/**
+ *
+ * @author grupa_1
+ */
+@Named(value = "lokalizator")
+@SessionScoped
+public class Lokalizacija implements Serializable {
+
+    static final ArrayList<Izbornik> izbornikJezika = new ArrayList<>();
+    String odabraniJezik;
+
+    static {
+        izbornikJezika.add(new Izbornik("hrvatski", "hr"));
+        izbornikJezika.add(new Izbornik("engleski", "en"));
+        izbornikJezika.add(new Izbornik("njemački", "de"));
+    }
+
+    /**
+     * Creates a new instance of Lokalizacija
+     */
+    public Lokalizacija() {
+    }
+
+    public String getOdabraniJezik() {
+        UIViewRoot view = FacesContext.getCurrentInstance().getViewRoot();
+        if (view == null) {
+            odabraniJezik = "hr";
+        } else {
+            Locale lokalniJezik = FacesContext.getCurrentInstance().getViewRoot().getLocale();
+            odabraniJezik = lokalniJezik.getLanguage();
+        }
+        return odabraniJezik;
+    }
+
+    public void setOdabraniJezik(String odabraniJezik) {
+        this.odabraniJezik = odabraniJezik;
+        Locale lokalniJezik = new Locale(odabraniJezik);
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(lokalniJezik);
+    }
+
+    public ArrayList<Izbornik> getIzbornikJezika() {
+        return izbornikJezika;
+    }
+
+    public Object odaberiJezik() {
+        setOdabraniJezik(odabraniJezik);
+        return "PromjenaJezika";
+    }
+}
